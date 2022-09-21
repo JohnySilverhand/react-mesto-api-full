88570e56446fccaa09fcc539e724538dfd9c2296
Андрей <abobus21111@gmail.ru>
2022-09-21 18:31:47 +0300

    fix

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,6 @@
+require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const { errors, Joi, celebrate } = require('celebrate');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -8,15 +10,26 @@ const cardRouter = require('./routes/cards');
 const SERVER_ERROR = require('./errors/status');
 const NotFound = require('./errors/error');
 const auth = require('./middlewares/auth');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
+app.use(cors());
+
 app.use(bodyParser.json());
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
+});
+
+app.use(requestLogger);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
 });
 
 app.post('/signin', celebrate({
@@ -41,6 +54,8 @@ app.use('/cards', cardRouter);
 app.use('*', (req, res, next) => {
   next(new NotFound('Страница не найдена'));
 });
+
+app.use(errorLogger);
 
 app.use(errors());
 
